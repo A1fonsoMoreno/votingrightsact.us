9565bd0ace191f6f24cecd7b8213ebd48a893ca6
Adam Cuppy <adam@codingzeal.com>
2014-06-26 14:58:31 -0700

    remote smarty streets

--- config/initializers/smarty_streets.rb
+++
@@ -1,9 +0,0 @@
-require "smarty_streets"
-
-secrets = Rails.application.secrets.smarty_streets
-
-SmartyStreets.configure do |c|
-  c.auth_id = secrets.fetch("auth_id")
-  c.auth_token = secrets.fetch("auth_token")
-  c.number_of_candidates = 5
-end
